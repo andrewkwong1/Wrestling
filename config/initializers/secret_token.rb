ff1ea87603735dd9a3bf97cc600f50e6d15594a5
Andrew Kwong <andrewthomaskwong@gmail.com>
2014-03-02 16:31:19 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wrestling::Application.config.secret_key_base = 'c0258d1b8455864d8bf7dcafceeb6922d4671ce015128a7428d49afa88b7107e2ad8bb72e943b42c6faf582ba21ebfa868eb97d425ac375365b1a463d6c799cb'
